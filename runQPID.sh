8aefa02c901ad6e4180d3a2959d79d3f77d71b96
root <root@node001.hspu.local>
2016-07-14 11:35:27 +0300

    Create admin user

--- runQPID.sh
+++ runQPID.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cat /usr/etc/qpid/qpidd.conf.dist | sed "s/DOMAIN/$DOMAIN/g" | sed "s/REALM/$REALM/g" >/usr/etc/qpid/qpidd.conf
 cat /usr/etc/qpid/qpidd.acl.dist | sed "s/REALM/$REALM/g" >/usr/etc/qpid/qpidd.acl
+rm /var/lib/qpidd/qpidd.sasldb
+echo admin | saslpasswd2 -c -p -f /var/lib/qpidd/qpidd.sasldb -u $REALM admin
 cd /var/lib/qpidd
 /usr/sbin/qpidd
